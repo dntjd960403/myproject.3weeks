c09994d890ba9ba2f700d6bc24c21b3d99cf642c
dntjd960403 <dntjd960403@gmail.com>
2022-10-03 14:58:14 +0900

    몽고 커넥트 위치 수정

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
-    .connect("mongodb://localhost:27017/myproject")
+    .connect("mongodb://localhost:27017/sparta_3weeks")
     .catch(err => console.log(err));
 };
 
